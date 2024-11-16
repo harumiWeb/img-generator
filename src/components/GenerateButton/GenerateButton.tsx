059a0aefcff72bd1defed86ec85346f5487dafbd
harumiWeb <ganaharumi@outlook.jp>
2024-11-16 14:16:35 +0900

    利用規約とプライバシーポリシーページを作成しました。

--- src/components/GenerateButton/GenerateButton.tsx
+++ src/components/GenerateButton/GenerateButton.tsx
@@ -12,7 +12,10 @@ export function GenerateButton({
   const [error, setError] = useState<string | null>(null);
 
   return (
-    <>
+    <div className="flex flex-col items-center justify-center">
+      <p className="text-center text-sm text-gray-500 pt-4 pb-6">
+        <a href="/terms" className="underline text-blue-500 font-bold">利用規約</a>と<a href="/privacy" className="underline text-blue-500 font-bold">プライバシーポリシー</a>に同意の上でご利用ください。
+      </p>
       <button
         className={`generate-button bg-blue-500 flex items-center justify-center gap-3 text-white text-lg font-bold py-2 px-8 rounded-md mx-auto hover:bg-blue-600 transition-colors duration-200 ${
           isGenerating ? "opacity-50 cursor-not-allowed" : ""
@@ -26,6 +29,6 @@ export function GenerateButton({
         {isGenerating ? "生成中..." : "imgタグを生成する"}
       </button>
       {error && <p className="text-red-500 text-center mt-4">{error}</p>}
-    </>
+    </div>
   );
 }
